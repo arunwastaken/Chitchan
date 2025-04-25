caa2842d36345c73a2a62d0e16636d7fc77d8aa3
anantnotfound <anantpsagar01@gmail.com>
2025-04-25 21:24:11 +0530

    Updated input.ts

--- src/chakra/input.ts
+++ src/chakra/input.ts
@@ -26,7 +26,7 @@ export const Input: ComponentStyleConfig = {
   sizes: {
     md: {
       field: {
-        // height: "30px",
+        // height: "30px"
         fontSize: "10pt",
       },
     },
