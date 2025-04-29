ca1af7d6f8a98fa3fbd7b772efed26072f7866c8
anantnotfound <anantpsagar01@gmail.com>
2025-04-29 10:07:45 +0530

    Defined interfaces for Community

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { RecoilRoot } from "recoil";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/theme.css";
 
+
 function App() {
   return (
     <RecoilRoot>
@@ -20,5 +21,6 @@ function App() {
     </RecoilRoot>
   );
 }
+
 
 export default App; 
