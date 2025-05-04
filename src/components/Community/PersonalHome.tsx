6c67c33be10b6c654e8184d234a514bd521daf55
arunwastaken <at4741289@gmail.com>
2025-05-04 13:16:03 +0530

    create community on home fixed

--- src/components/Community/PersonalHome.tsx
+++ src/components/Community/PersonalHome.tsx
@@ -1,47 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Flex, Icon, Stack, Text, useColorMode } from "@chakra-ui/react";
 import { ChitchanLogoColored } from "../../components/Icons/ChitchanLogoColored";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/clientApp";
+import CreateCommunityModal from "../Modal/CreateCommunity";
 
 const PersonalHome: React.FC = () => {
   const { colorMode } = useColorMode();
-  
+  const [open, setOpen] = useState(false);
+  const [user] = useAuthState(auth);
+
   return (
-    <Flex
-      direction="column"
-      bg={colorMode === "dark" ? "dark.card" : "white"}
-      borderRadius={4}
-      cursor="pointer"
-      border="1px solid"
-      borderColor={colorMode === "dark" ? "dark.300" : "gray.300"}
-      position="sticky"
-    >
+    <>
       <Flex
-        align="flex-end"
-        color="white"
-        p="6px 10px"
-        bg="blue.500"
-        height="34px"
-        borderRadius="4px 4px 0px 0px"
-        fontWeight={600}
-        bgImage="url(/images/redditPersonalHome.png)"
-        backgroundSize="cover"
-      />
-      <Flex direction="column" p="12px">
-        <Flex align="center" mb={2}>
-          <Icon as={ChitchanLogoColored} fontSize={50} mr={2} />
-          <Text fontWeight={600}>Home</Text>
+        direction="column"
+        bg={colorMode === "dark" ? "dark.card" : "white"}
+        borderRadius={4}
+        cursor="pointer"
+        border="1px solid"
+        borderColor={colorMode === "dark" ? "dark.300" : "gray.300"}
+        position="sticky"
+      >
+        <Flex
+          align="flex-end"
+          color="white"
+          p="6px 10px"
+          bg="blue.500"
+          height="34px"
+          borderRadius="4px 4px 0px 0px"
+          fontWeight={600}
+          bgImage="url(/images/redditPersonalHome.png)"
+          backgroundSize="cover"
+        />
+        <Flex direction="column" p="12px">
+          <Flex align="center" mb={2}>
+            <Icon as={ChitchanLogoColored} fontSize={50} mr={2} />
+            <Text fontWeight={600}>Home</Text>
+          </Flex>
+          <Stack spacing={3}>
+            <Text fontSize="9pt">
+              Your personal Chitchan frontpage, built for you.
+            </Text>
+            <Button height="30px">Create Post</Button>
+            <Button variant="outline" height="30px" onClick={() => setOpen(true)}>
+              Create Community
+            </Button>
+          </Stack>
         </Flex>
-        <Stack spacing={3}>
-          <Text fontSize="9pt">
-            Your personal Chitchan frontpage, built for you.
-          </Text>
-          <Button height="30px">Create Post</Button>
-          <Button variant="outline" height="30px">
-            Create Community
-          </Button>
-        </Stack>
       </Flex>
-    </Flex>
+      <CreateCommunityModal isOpen={open} handleClose={() => setOpen(false)} userId={user?.uid || ""} />
+    </>
   );
 };
 
